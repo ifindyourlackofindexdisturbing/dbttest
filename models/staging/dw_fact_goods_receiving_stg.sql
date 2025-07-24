bdf8966c7a7c6e3da725d6f8f0a64ed64fb960b1
ofer sharir <ofer.sharir@innoviz-tech.com>
2025-07-24 22:37:35 +0000

    changing the protocol in the ADLS adress string

--- models/staging/dw_fact_goods_receiving_stg.sql
+++ models/staging/dw_fact_goods_receiving_stg.sql
@@ -1,9 +1,6 @@
 {{config(
     materialized = 'table',
-    location = 'wasbs://databricks-output@innovizadls.blob.core.windows.net/staging/dw_fact_goods_recieving_stg/',
+    location = 'abfss://databricks-output@innovizadls.dfs.core.windows.net/staging/dw_fact_goods_recieving_stg/',
     external=True
-)
-
-
-}}
+)}}
 select 'do or do not, there is no try' as favorite_quote
